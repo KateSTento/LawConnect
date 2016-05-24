295f0ce48688e39cb339e41e5c4431d0d8851484
KateSTento <bktento@skymesh.com.au>
2016-05-24 00:55:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lawconnect::Application.config.secret_key_base = '055bef5df95659cb611d9d2fa1f772c7cb21915e95a3984f9593486a4f9eb269475ff601527028d1cdbf806bbfd8e642c178338351a3da2b22ee5808a0eb8441'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lawconnect::Application.config.session_store :cookie_store, key: '_lawconnect_session'
